bac33b3077b680b87004e97abcdc301df5bfbb02
LUCIAN VALENTIN PIRONE <lucian.pirone@ro.ibm.com>
2023-01-26 14:47:55 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { 
+  Pets 
+} from './ui-components';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <Pets />
+      </header>
+    </div>
+  );
+}
+
+export default App;
